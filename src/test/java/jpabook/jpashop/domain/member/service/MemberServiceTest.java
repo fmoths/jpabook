ce1c2d966fe40e55a79659654f8c10681c16de6f
kdpark <kdpark1623@gmail.com>
2021-01-10 22:14:38 +0900

    feat: MemberService 생성

---
+++ src/test/java/jpabook/jpashop/domain/member/service/MemberServiceTest.java
@@ -0,0 +1,41 @@
+package jpabook.jpashop.domain.member.service;
+
+
+import jpabook.JpaBookApplication;
+import jpabook.jpashop.domain.member.entity.Member;
+import jpabook.jpashop.domain.member.repository.MemberRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = JpaBookApplication.class)
+@Transactional
+public class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @DisplayName("회원가입을 신청하면 유저의 데이터가 저장된다.")
+    public void join() {
+        //given
+        Member member = new Member();
+        member.setName("kim");
+
+        //when
+        Long saveId = memberService.join(member);
+
+        //then
+        assertEquals(member, memberRepository.findById(saveId));
+    }
+}
