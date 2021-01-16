fb2efb2e3e052d3ee56f26f94481a48a9aee3ad7
박경덕 <kdpark@baggyeongdeog-ui-MacBookAir.local>
2021-01-16 23:42:37 +0900

    feat: handlebar 라이브러리 추가

--- src/main/java/jpabook/jpashop/domain/item/ItemController.java
+++ src/main/java/jpabook/jpashop/domain/item/ItemController.java
@@ -2,17 +2,29 @@ package jpabook.jpashop.domain.item;
 
 
 import jpabook.jpashop.domain.item.service.ItemService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 //TODO::handlebar 사용하여 뷰 만들어야 함..
-@RestController("/items")
+@Slf4j
+@Controller("/items")
 public class ItemController {
 
     @Autowired
     private ItemService itemService;
+
+    @GetMapping
+    public String list(Model model){
+        List<Item> items = itemService.findItems();
+        model.addAttribute("items", items);
+        return "items/itemList";
+    }
 
     @PostMapping("/new")
     public String createItem(Item item) {
