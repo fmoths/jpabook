45074756f8bd09b0287f801edae54be81674fa78
fmoths <kdpark1623@gmail.com>
2021-01-17 16:01:01 +0900

    feat: ItemController Test 코드 추가

--- src/main/java/jpabook/JpaBookApplication.java
+++ src/main/java/jpabook/JpaBookApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JpaBookApplication {
-
     public static void main(String [] args){
         SpringApplication.run(JpaBookApplication.class, args);
     }
--- src/test/java/jpabook/jpashop/domain/item/ItemControllerTest.java
+++ src/test/java/jpabook/jpashop/domain/item/ItemControllerTest.java
@@ -1,4 +1,28 @@
-import static org.junit.jupiter.api.Assertions.*;
+package jpabook.jpashop.domain.item;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
 class ItemControllerTest {
-  
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @Test
+    @DisplayName("/items로 접속하면 상품리스트를 보여준다.")
+    public void accessToItemListPage(){
+        //when
+        String body = this.testRestTemplate.getForObject("/items", String.class);
+        assertThat(body).contains("<title>상품 리스트</title>");
+    }
 }
